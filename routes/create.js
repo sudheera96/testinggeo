c3d6efd55caf998b9fb84c2dcd33fdfd7125f44c
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-03-17 01:10:16 -0500

    updated

--- routes/create.js
+++ routes/create.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+    res.render('create', { title: 'Create' });
 });
 
 module.exports = router;
